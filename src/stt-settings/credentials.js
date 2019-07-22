2e365562456421597622daece4accf472d2b2592
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-07-22 17:16:29 +0100

    credentials in settings view

--- src/stt-settings/credentials.js
+++ src/stt-settings/credentials.js
@@ -1,40 +1,58 @@
 const fs = require('fs');
 const path = require('path');
 const electron = require('electron');
-const appUserDataPath = electron.remote.app.getPath("userData");
+const appUserDataPath = electron.remote.app.getPath('userData');
 
 const credentialsTemplate = {
-    provider: '',
-    sttUserName: '',
-    sttAPIKey: '',
-    sttAPIUrl: ''
+  provider: '',
+  sttUserName: '',
+  sttAPIKey: '',
+  sttAPIUrl: ''
+};
+
+function deepCopy(data) {
+  return JSON.parse(JSON.stringify(data));
+}
+
+function getCredentialsFilePath(provider) {
+  return path.join(appUserDataPath, `${ provider }.json`);
+}
+
+function setCredentials (data) {
+  fs.writeFileSync(getCredentialsFilePath(data.provider), JSON.stringify(data, null, 2));
+}
+
+function getCredentials (provider) {
+  let credentials = deepCopy(credentialsTemplate);
+  credentials.provider = provider;
+  const credentialsFilePath = getCredentialsFilePath(provider);
+
+  if (fs.existsSync(credentialsFilePath)) {
+    credentials = JSON.parse(fs.readFileSync(credentialsFilePath).toString());
+
+    return credentials;
   }
-
-function deepCopy(data){
-    return JSON.parse(JSON.stringify(data))
+  else {
+    return credentials;
+  }
 }
 
-function getCredentialsFilePath(provider){
-    return path.join(appUserDataPath, `${provider}.json`)
+function areCredentialsSet(provider) {
+  const credentials = getCredentials(provider);
+  switch (provider) {
+  case 'AssemblyAI':
+    return credentials.sttAPIKey !== '';
+  case 'Speechmatics':
+    return credentials.sttUserName !== '' && credentials.sttAPIKey !== '';
+  case 'BBC':
+    return credentials.sttUserName !== '';
+  default:
+    console.error(`Could not find credentials for provier: ${ provider }`);
+
+    return false;
+  }
 }
 
-function getCredentials (provider){
-    let credentials = deepCopy(credentialsTemplate);
-    credentials.provider = provider;
-    const credentialsFilePath = getCredentialsFilePath(provider);
-
-    if(fs.existsSync(credentialsFilePath)){
-        credentials = JSON.parse(fs.readFileSync(credentialsFilePath).toString());
-        // console.log('fs.existsSync(credentialsFilePath)', credentials);
-        return credentials;
-    }
-    else{
-        return credentials;
-    }    
-}
-
-function setCredentials (data){
-    fs.writeFileSync(getCredentialsFilePath(data.provider), JSON.stringify(data,null,2));
-}
-module.exports.getCredentials = getCredentials
-module.exports.setCredentials = setCredentials
+module.exports.setCredentials = setCredentials;
+module.exports.getCredentials = getCredentials;
+module.exports.areCredentialsSet = areCredentialsSet;
