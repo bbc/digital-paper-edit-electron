bcabb505a9eb4ef9636dde1ae068df2850d2cbf3
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-07-31 20:29:07 +0100

    integrated with transcript editor
    can noow edit text, realign, and save from transcript editor to use in DPE, also updated build for client, client can be pulled into node modules via npm, but at the moment manually moving to build folder via copy and paste, need to automate this step, either by copying folder from node modules on packaging/dev/prod or by changing index.html in react client where it points to react client to be getting to right path out of node modules. or rething how electron consumes the client in prod and development.

---
+++ build/precache-manifest.b4c3fe5cecb12d55742d69df61bfd0b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a2326cf3802a4e3ee80b04509dad56",
+    "url": "./index.html"
+  },
+  {
+    "revision": "83b804face62908d985f",
+    "url": "./static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "c2d00a96cc25a959c3d4",
+    "url": "./static/css/main.cbfc5536.chunk.css"
+  },
+  {
+    "revision": "83b804face62908d985f",
+    "url": "./static/js/2.231bac4f.chunk.js"
+  },
+  {
+    "revision": "c2d00a96cc25a959c3d4",
+    "url": "./static/js/main.b23a7ee7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "658ac9e377b9bde30bb2aec15fa29151",
+    "url": "./static/media/labsLogo.658ac9e3.png"
+  }
+]);
