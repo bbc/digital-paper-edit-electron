53624453ffc078677e8dcc6be8cb8d5a88dc5575
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-07-31 23:56:36 +0100

    1.0.8

---
+++ build/precache-manifest.6a3fcf1fffe82c27722a04d7e2bebb30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0a4ff88fe7bb19a0c9e2578d0c0fda",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27ff0f2bfa5887bda589",
+    "url": "./static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "198a8c5237fdb647cf1e",
+    "url": "./static/css/main.cbfc5536.chunk.css"
+  },
+  {
+    "revision": "27ff0f2bfa5887bda589",
+    "url": "./static/js/2.23b763bf.chunk.js"
+  },
+  {
+    "revision": "198a8c5237fdb647cf1e",
+    "url": "./static/js/main.bdf811a1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "658ac9e377b9bde30bb2aec15fa29151",
+    "url": "./static/media/labsLogo.658ac9e3.png"
+  }
+]);
